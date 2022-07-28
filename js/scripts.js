79bb1cbf7b2f0a8ccd9876d089934e6ed4e18e15
Adrian <coneaconea5@gmail.com>
2022-07-28 15:25:20 +0300

    tiras-building-designe

---
+++ js/scripts.js
@@ -0,0 +1,24 @@
+
+
+          //Burger
+  $(".katalog").click(function (event) {
+    
+    $(".elements-nav").toggleClass("menu_active");
+    $(".general").toggleClass("nav_active");
+    $(".all-element").toggleClass("navbar_active");
+    $("body").toggleClass("body_active");
+  });
+
+  $(".close").click(function (event) {   
+    $(".elements-nav").toggleClass("menu_active");
+    $(".general").toggleClass("nav_active");
+    $(".all-element").toggleClass("navbar_active");
+    $("body").toggleClass("body_active");
+  });
+
+$(".chat-mes").click(function (event) {
+  $(".pixel").show()
+  
+
+})
+
